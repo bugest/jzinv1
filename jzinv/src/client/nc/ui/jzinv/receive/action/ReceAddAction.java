badf34a138c78dd0a9f59471baa15d1af37784c1
李楠 <linanb@yonyou.com>
2017-10-25 16:10:50 +0800

    112

---
+++ jzinv/src/client/nc/ui/jzinv/receive/action/ReceAddAction.java
@@ -0,0 +1,58 @@
+package nc.ui.jzinv.receive.action;
+
+import nc.bs.framework.common.NCLocator;
+import nc.itf.jzinv.invpub.IJzinvQuery;
+import nc.ui.jzinv.pub.action.InvoiceAction;
+import nc.ui.trade.manage.BillManageUI;
+import nc.vo.jzinv.pub.IJzinvBillType;
+import nc.vo.jzinv.receive.ReceiveVO;
+import nc.vo.jzinv.vat0505.VatTaxorgsetVO;
+/**
+ * 收票新增按钮
+ * @author mayyc
+ *
+ */
+public class ReceAddAction extends InvoiceAction{
+
+	public ReceAddAction(BillManageUI clientUI) {
+		super(clientUI);
+	}
+
+	@Override
+	public void doAction() throws Exception {
+		getClientUI().getBillCardPanel().getHeadTabbedPane().setSelectedIndex(0);
+		String pk_corp = getClientUI()._getCorp().getPk_corp(); 
+		//IJzinvBillType.JZINV_RECEIVE_MT
+		boolean bisupload = nc.vo.jzinv.param.InvParamTool.isToGLByBilltype(pk_corp, getClientUI().getUIControl().getBillType());
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.BISUPLOAD).setValue(bisupload);
+		setAuthenOrg();
+		setRedRelateField();
+		//新增时颜色控制 linan
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.NTOTALINVOICEAMOUNTMNY).setEdit(false);
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.NTOTALINVOICEAMOUNTTAXMNY).setEdit(false);
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.NTOTALINVOICETAX).setEdit(false);
+	}
+	private void setRedRelateField(){
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.PK_RECEIVE_REF).setEdit(false);
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.PK_RECEIVE_REF).setNull(false);
+		
+//		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.IREDAPLYREASON).setEdit(false);
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.IREDAPLYREASON).setNull(false);
+		
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.VREDINFONO).setEdit(false);
+		getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.VREDINFONO).setNull(false);
+	}
+	/*
+	 * 设置认证财务组织
+	 */
+	private void setAuthenOrg() throws Exception{
+		String pk_corp = getClientUI()._getCorp().getPk_corp();
+		IJzinvQuery query = NCLocator.getInstance().lookup(IJzinvQuery.class);
+		VatTaxorgsetVO orgSetVO = query.getTaxOrg(pk_corp);
+		if(null != orgSetVO){
+			getClientUI().getBillCardPanel().setHeadItem(ReceiveVO.PK_FINANCE, orgSetVO.getPk_taxorg());
+			getClientUI().getBillCardPanel().setHeadItem(ReceiveVO.VTAXPAYERNUMBER, orgSetVO.getVtaxpayernumber());
+		    
+		}
+	}
+}
