0593db1ff8e0eebd54bfe0c86fc5edbd64cac1b2
李楠 <linanb@yonyou.com>
2017-10-26 09:47:45 +0800

    票面总税金编辑后逻辑

--- jzinv/src/client/nc/ui/jzinv/receive/handler/NtotalinvoicetaxAfterEdit.java
+++ jzinv/src/client/nc/ui/jzinv/receive/handler/NtotalinvoicetaxAfterEdit.java
@@ -1,9 +1,15 @@
 package nc.ui.jzinv.receive.handler;
 
+import java.util.List;
+
+import nc.bs.framework.common.NCLocator;
+import nc.bs.logging.Logger;
+import nc.itf.jzinv.receive.IReceiveService;
 import nc.ui.jzinv.pub.handler.InvCardEditHandler;
 import nc.ui.pub.bill.BillEditEvent;
 import nc.ui.trade.manage.BillManageUI;
 import nc.vo.jzinv.receive.ReceiveVO;
+import nc.vo.pub.BusinessException;
 import nc.vo.pub.lang.UFBoolean;
 import nc.vo.pub.lang.UFDouble;
 
@@ -27,22 +33,47 @@ public class NtotalinvoicetaxAfterEdit extends InvCardEditHandler {
 					.toString());
 			// 如果没选中的话，就不处理逻辑
 			if (UFBoolean.TRUE.equals(bissplit)) {
-				//总税金
+				// 总税金
 				UFDouble ntotalinvoicetax = e.getValue() == null ? UFDouble.ZERO_DBL
 						: new UFDouble(e.getValue().toString());
-				//本次拆分税金
+				// 本次拆分税金
 				UFDouble ntaxmny = getClientUI().getBillCardPanel()
 						.getHeadItem(ReceiveVO.NTAXMNY).getValueObject() == null ? UFDouble.ZERO_DBL
 						: new UFDouble(getClientUI().getBillCardPanel()
 								.getHeadItem(ReceiveVO.NTAXMNY)
 								.getValueObject().toString());
-				//设置累计已拆分金额
-				UFDouble sumTax = null; //todo
-				//设置累计已拆分金额
-				getClientUI().getBillCardPanel().getHeadItem(ReceiveVO.NCUMULATIVESPLITTAX).setValue(sumTax);
-				//设置剩余拆分税金
+				// 当前的表id
+				String pk_receive = (String) getClientUI().getBillCardPanel()
+						.getHeadItem(ReceiveVO.PK_RECEIVE).getValueObject();
+				String vinvcode = (String) getClientUI().getBillCardPanel()
+						.getHeadItem(ReceiveVO.VINVCODE).getValueObject();
+				String vinvno = (String) getClientUI().getBillCardPanel()
+						.getHeadItem(ReceiveVO.VINVNO).getValueObject();
+				// 设置累计已拆分金额
+				UFDouble sumTax = UFDouble.ZERO_DBL; // todo
+				try {
+					List<ReceiveVO> receiveVOList = NCLocator
+							.getInstance()
+							.lookup(IReceiveService.class)
+							.querySplitHeadVOsByCond(vinvcode, vinvno,
+									pk_receive);
+					if (receiveVOList != null && !receiveVOList.isEmpty()) {
+						for (ReceiveVO receiveVO : receiveVOList) {
+							sumTax = sumTax.add(receiveVO.getNtaxmny() == null ? UFDouble.ZERO_DBL : receiveVO.getNtaxmny());
+						}
+					}
+				} catch (BusinessException be) {
+					Logger.error("查询发票拆分情况报错！", be);
+					getClientUI().showErrorMessage("查询发票拆分情况报错!");
+				}
+				// 设置累计已拆分金额
 				getClientUI().getBillCardPanel()
-						.getHeadItem(ReceiveVO.NSURPLUSSPLITTAX).setValue(ntotalinvoicetax.sub(ntaxmny).sub(sumTax));		
+						.getHeadItem(ReceiveVO.NCUMULATIVESPLITTAX)
+						.setValue(sumTax);
+				// 设置剩余拆分税金
+				getClientUI().getBillCardPanel()
+						.getHeadItem(ReceiveVO.NSURPLUSSPLITTAX)
+						.setValue(ntotalinvoicetax.sub(ntaxmny).sub(sumTax));
 			}
 		}
 	}
